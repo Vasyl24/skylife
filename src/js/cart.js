4c8b50d48c0aed2327e251455c067360b7b9764b
Vasyl Shaposhnyk <aries240302@gmail.com>
2023-05-28 23:29:46 +0300

    Edit shopping cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,12 +1,6 @@
-// import icon from '../images/icon.svg';
+import icon from '../images/icon.svg';
 import image from '../images/empty-cart.png';
 
-if (
-  window.location.pathname === '/shopping-cart.html' &&
-  window.screen.width < 1440
-) {
-  refs.supportUkraineAside.style.display = 'none';
-}
 // if (window.location.pathname === '/shopping-cart.html') {
 //   refs.headerHomeBtn.classList.remove('current');
 //   refs.headerShoppingListBtn.classList.add('current');
@@ -15,40 +9,47 @@ if (
 const slPage = document.querySelector('.js-sl');
 const emptyCart = `<div class="empty-cart">
     <p class="sl-message">Ця сторінка порожня, додайте якийсь товар в кошик, щоб замовити.</p>
-    <img src="${image}" alt="books" width="300"/>
+    <img src="${image}" alt="products" width="300"/>
   </div>`;
 
 // Getting data from local Storage
-let slBooksData = JSON.parse(localStorage.getItem('books'));
+let slProductsData = JSON.parse(localStorage.getItem('products'));
 
 function renderSlPage() {
-  if (!slBooksData || slBooksData === []) {
+  if (!slProductsData || slProductsData === []) {
     slPage.innerHTML = emptyCart;
   } else {
-    slPage.innerHTML = createCardMarkup(slBooksData);
-    const removeBtn = slPage.querySelectorAll('.js-remove-book');
-    removeBtn.forEach(btn => btn.addEventListener('click', removeBookFromCart));
+    slPage.innerHTML = createCardMarkup(slProductsData);
+    const removeBtn = slPage.querySelectorAll('.js-remove-product');
+    removeBtn.forEach(btn =>
+      btn.addEventListener('click', removeProductFromCart)
+    );
   }
-  if (slBooksData) {
-    slPage.innerHTML = createCardMarkup(slBooksData);
-    const removeBtn = slPage.querySelectorAll('.js-remove-book');
-    removeBtn.forEach(btn => btn.addEventListener('click', removeBookFromCart));
+  if (slProductsData) {
+    slPage.innerHTML = createCardMarkup(slProductsData);
+    const removeBtn = slPage.querySelectorAll('.js-remove-product');
+    removeBtn.forEach(btn =>
+      btn.addEventListener('click', removeProductFromCart)
+    );
   } else {
     slPage.innerHTML = emptyCart;
   }
 }
 renderSlPage();
 
-//  Removing a book from shopping list
-
-function removeBookFromCart(event) {
+//  Removing a product from shopping list
+function removeProductFromCart(event) {
   const slCard = event.target.closest('.sl-card');
+
   slCard.classList.add('removing');
   slCard.addEventListener('transitionend', () => {
-    const slBookId = slCard.dataset.id;
-    slBooksData = slBooksData.filter(book => book._id !== slBookId);
-    localStorage.setItem('books', JSON.stringify(slBooksData));
-    if (!slBooksData.length) {
+    const slProductId = slCard.dataset.id;
+
+    slProductsData.filter(product => product.id !== slProductId);
+    // console.log(slProductsData);
+    localStorage.setItem('products', JSON.stringify(slProductsData));
+    // console.log(localStorage);
+    if (!slProductsData.length) {
       slPage.innerHTML = emptyCart;
     } else {
       renderSlPage();
@@ -57,48 +58,38 @@ function removeBookFromCart(event) {
 }
 // Creating markup function
 
-function createCardMarkup(booksData) {
-  return booksData
-    .map(
-      ({
-        _id,
-        book_image,
-        description,
-        author,
-        list_name,
-        title,
-        buy_links,
-      }) => {
-        const amazonUrl = buy_links[0].url;
-        const iBooksUrl = buy_links[1].url;
-        const bookshopUrl = buy_links[4].url;
+function createCardMarkup(productsData) {
+  return productsData
+    .map(({ id, img, name, price, description }) => {
+      // const amazonUrl = buy_links[0].url;
+      // const iBooksUrl = buy_links[1].url;
+      // const productshopUrl = buy_links[4].url;
 
-        return `<li class="sl-card" data-id=${_id}>
-      <img src="${book_image}" alt="${title}" class="sl-book-img">
-      <div class="sl-book-info">
-          <h3 class="sl-book-title">${title}</h3>
-          <p class="sl-book-cat">${list_name}</p>
-          <p class="sl-book-descr">${description}</p>
+      return `<li class="sl-card" data-id=${id}>
+      <img src="${img}" alt="${name}" width="290" height="170" class="sl-product-img">
+      <div class="sl-product-info">
+          <h3 class="sl-product-title">${name}</h3>
+          <h2 class="sl-product-cat">${price}</h2>
+          <p class="sl-product-descr">${description}</p>
           <div class="sl-aut-shops">
-            <p class="sl-book-author">${author}</p>
+            <p class="sl-product-author">${name}</p>
             <ul class="sl-eshops">
-              <li><a target="_blank" href="${amazonUrl}">
+              <li><a target="_blank" href="#">
                       <svg class="sl-amazon" width="32px" height="11px">
-                      <use href="${icon}#icon-amazon"></use></svg></a></li>
-              <li><a target="_blank" href="${iBooksUrl}"><svg class="sl-ibooks" width="16px" height="16px">
-                      <use href="${icon}#icon-ibooks"></use></svg></a></li>
-              <li><a target="_blank" href="${bookshopUrl}"><svg class="sl-bookshop" width="16px" height="16px">
-                      <use href="${icon}#icon-bookshop"></use></svg></a></li>
+                      <use href="#"></use></svg></a></li>
+              <li><a target="_blank" href="#"><svg class="sl-ibooks" width="16px" height="16px">
+                      <use href="#"></use></svg></a></li>
+              <li><a target="_blank" href="#"><svg class="sl-bookshop" width="16px" height="16px">
+                      <use href="#"></use></svg></a></li>
             </ul>
           </div>
       </div>
-      <button type="button" class="trash-btn js-remove-book">
+      <button type="button" class="trash-btn js-remove-product">
       <svg width="16px" height="16px">
       <use href="${icon}#icon-trash"></use></svg>
       </button>
       
     </li>`;
-      }
-    )
+    })
     .join('');
 }
