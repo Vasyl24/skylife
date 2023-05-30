5b3017e39143f1c384fef445ebe06414861c9d68
Vasyl Shaposhnyk <aries240302@gmail.com>
2023-05-30 16:18:05 +0300

    Edit swiper

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,21 +1,18 @@
 import Swiper from 'swiper';
-import 'swiper/swiper.min.css';
+import 'swiper/swiper-bundle.min.css';
+import Swiper, { Navigation } from 'swiper';
+
+Swiper.use([Navigation]);
 
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   direction: 'horizontal',
   slidesPerView: 4,
   loop: true,
   spaceBetween: 10,
-  // Navigation arrows
+
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
-  // pagination: {
-  //   el: '.swiper-pagination',
-  //   type: 'bullets',
-  // },
 });
 export { swiper };
