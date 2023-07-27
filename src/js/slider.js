b90ccee7cebdfb0932b8a7cf36e785d2005b0e9b
Vasyl Shaposhnyk <aries240302@gmail.com>
2023-07-27 14:17:52 +0300

    Add responsive styles

--- src/js/slider.js
+++ src/js/slider.js
@@ -10,6 +10,29 @@ const swiper1 = new Swiper('.swiper-1', {
   loop: true,
   spaceBetween: 20,
 
+  // breakpoints: {
+  // when window width is >= 320px
+  // 320: {
+  //   slidesPerView: 1,
+  //   spaceBetween: 20,
+  // },
+  // when window width is >= 480px
+  // 480: {
+  //   slidesPerView: 2,
+  //   spaceBetween: 30,
+  // },
+  // when window width is >= 640px
+  // 768: {
+  //   slidesPerView: 3,
+  //   spaceBetween: 20,
+  // },
+
+  // 1200: {
+  //   slidesPerView: 4,
+  //   spaceBetween: 20,
+  // },
+  // },
+
   navigation: {
     nextEl: '.swiper-button-next-1',
     prevEl: '.swiper-button-prev-1',
@@ -25,6 +48,24 @@ const swiper2 = new Swiper('.swiper-2', {
   loop: true,
   spaceBetween: 20,
 
+  // breakpoints: {
+  //   // when window width is >= 320px
+  //   320: {
+  //     slidesPerView: 1,
+  //     spaceBetween: 20,
+  //   },
+  //   // when window width is >= 480px
+  //   480: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 30,
+  //   },
+  //   // when window width is >= 640px
+  //   768: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 20,
+  //   },
+  // },
+
   navigation: {
     nextEl: '.swiper-button-next-2',
     prevEl: '.swiper-button-prev-2',
@@ -39,6 +80,24 @@ const swiper3 = new Swiper('.swiper-3', {
   slidesPerView: 4,
   loop: true,
   spaceBetween: 20,
+
+  // breakpoints: {
+  //   // when window width is >= 320px
+  //   320: {
+  //     slidesPerView: 1,
+  //     spaceBetween: 20,
+  //   },
+  //   // when window width is >= 480px
+  //   480: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 30,
+  //   },
+  //   // when window width is >= 640px
+  //   768: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 20,
+  //   },
+  // },
 
   navigation: {
     nextEl: '.swiper-button-next-3',
